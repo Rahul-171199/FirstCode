dc575c633daba8ed4c023b671e6e544b1aaa71c3
Rahul-171199 <rahulumate999@gmail.com>
2024-06-25 20:30:27 +0530

    Adding Maven Project

---
+++ src/main/java/com/springboot/webproject/controller/WebController.java
@@ -0,0 +1,17 @@
+package com.springboot.webproject.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+	
+	@PostMapping("/")
+	public String saveWebInfo()
+	{
+		
+		
+		return "this info is saved";
+		
+	}
+}
